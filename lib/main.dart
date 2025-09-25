dcd124f0c3fdec55d64c8db66a912bdc335b5cd4
korayguenbal <kguenbal@gmail.com>
2025-09-25 13:03:49 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: AlignmentGeometry.topLeft,
+              end: AlignmentGeometry.bottomRight,
+              colors: [Colors.yellow, Colors.red],
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset("assets/images/quiz-logo.png", width: 300),
+          SizedBox(height: 80),
+          Text("Quiz App!"),
+        ],
+      ),
+    );
+  }
+}
