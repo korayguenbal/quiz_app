afa8c574e33e5c7ab8ec12abb14722194bf95c8f
korayguenbal <kguenbal@gmail.com>
2025-09-30 12:28:01 +0200

    initState

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -17,12 +19,17 @@ class StartScreen extends StatelessWidget {
           SizedBox(height: 80),
           Text(
             "Quiz App!",
-            style: TextStyle(color: Colors.white, fontSize: 24),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
           ),
           SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {},
-            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
             icon: Icon(Icons.question_answer_outlined),
             label: Text(
               "Start Quiz",
